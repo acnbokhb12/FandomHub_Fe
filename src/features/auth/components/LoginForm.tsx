cbf4e1e30980c1905d87868160b42547d2657449
HuynhNgocKhanh <huynhngockhanh1201@gmail.com>
2025-06-04 11:06:38 +0700

    Update SiginForm

---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,10 @@
+
+const LoginfForm = () => {
+  return (
+    <div>
+      <h2>Login Form</h2>
+      {/* Add your login form implementation here */}
+    </div>
+  );
+}
+export default LoginfForm;
